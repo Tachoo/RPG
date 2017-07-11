e451f42614e3a97be1a15a93b7d2a97f6e4318a9
Sergio Anastacio Angulo Rosales <senao28@icloud.com>
2017-07-11 09:43:57 -0500

    update
    lol basura

---
+++ Assets/Scripts/Scenes/Menus/AccMenu/ACCSingleton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ACCSingleton : MonoBehaviour
+{
+    
+    private static ACCSingleton _instance;
+    public static ACCSingleton instance
+    {
+        //Funcion get 
+        get
+        {
+            if (_instance == null) //si  no hay nadaa pues XD lo creamos
+            {
+                GameObject _PlayerDataManager = new GameObject("_PlayerDataManager");
+                _PlayerDataManager.AddComponent<ACCSingleton>(); //Le a;adimos  este scrip al objeto
+                //Hacking
+                
+                //
+            }
+            return _instance; //Retornamos el objeto que esta como privado
+        }
+
+    }
+    void Awake()
+    {
+      
+        DontDestroyOnLoad(this.gameObject);
+        _instance = this;
+
+    }
+
+    /*Cuales datos me interesa saber del juegador*/
+
+    public bool IsAlive { get; set; }
+    public bool IsAFK { get; set; }
+    //public Haking Hack_Skill= _instance.GetComponent<Haking>();
+
+
+
+}
